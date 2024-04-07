4bada78941ac871e4fc421ad395cafdb2cb77a74
Tandamba Victoria <victoria.tandamba@etudiant.univ-lr.fr>
2024-04-07 21:24:38 +0200

    init models

--- src/todotask/models.py
+++ src/todotask/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# class Category(models.Model):
+#     name = models.CharField(max_length=100)
+#     slug = models.SlugField()
+
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    #category = models.ForeignKey(Category, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
